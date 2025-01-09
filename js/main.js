02f99a2a9b56179f0117980f594c36e8d72c1157
Trevor Marsh <tcmarsh@utah.gov>
2025-01-09 13:52:38 -0700

    Update to use server functionality

---
+++ js/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(function() {
+    function initialize() {
+        document.getElementById('offline_indicator').hidden = true;
+        document.querySelectorAll('.status')
+            .forEach(button => button.addEventListener('click', statusListener));
+
+        window.addEventListener('offline', () => {
+            document.getElementById('offline_indicator').hidden = false;
+        });
+        window.addEventListener('online', () => {
+            document.getElementById('offline_indicator').hidden = true;
+        });
+    }
+
+    function statusListener(e) {
+        let elem = document.getElementById('save_time');
+        let status = e.target.value;
+
+        fetch('/rest/time')
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                }
+                else {
+                    throw response.statusText;
+                }
+            })
+            .then(json => {
+                elem.innerText = `${ status } / Server time: ${ json.time }`;
+            })
+            .catch(reason => {
+                elem.innerText = `Error getting date from server '${ reason }'`;
+            });
+    }
+
+    initialize();
+})();
